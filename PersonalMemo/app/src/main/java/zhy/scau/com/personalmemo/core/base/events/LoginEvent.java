fc8182c38c2aeb1acf7d7b129a06599efaf57b67
zhenghy <1302723731@qq.com>
2017-02-16 13:31:56 +0800

    [其他]全局事件 -登录-登出

---
+++ PersonalMemo/app/src/main/java/zhy/scau/com/personalmemo/core/base/events/LoginEvent.java
@@ -0,0 +1,12 @@
+package zhy.scau.com.personalmemo.core.base.events;
+
+import zhy.scau.com.personalmemo.core.base.bean.BaseEntity;
+
+/**
+ * Created by ZhengHy on 2017-02-16.
+ * <p>
+ * 登录事件
+ */
+
+public class LoginEvent extends BaseEntity{
+}
---
+++ PersonalMemo/app/src/main/java/zhy/scau/com/personalmemo/core/base/events/LogoutEvent.java
@@ -0,0 +1,10 @@
+package zhy.scau.com.personalmemo.core.base.events;
+
+import zhy.scau.com.personalmemo.core.base.bean.BaseEntity;
+
+/**
+ * Created by ZhengHy on 2017-02-16.
+ */
+
+public class LogoutEvent extends BaseEntity{
+}
